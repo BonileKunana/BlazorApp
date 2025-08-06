ca9c4064c9b24fc95d60b75578e91a788c292f5e
Bonile <kunanabonile@gmail.com>
2025-08-06 22:36:27 +0200

    added a todo list

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b875c016696448dd0737862b78ce67c8439a41c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26ffb6656e9deb2446238221fe6dd8396537b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
